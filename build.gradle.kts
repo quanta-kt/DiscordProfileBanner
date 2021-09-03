04efa7d8b2fd128d1a039ed0442df16f42848481
abhijeet <abhijeet.nkt@gmail.com>
2021-09-03 23:12:26 +0530

    Log visitors

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,13 +18,23 @@ repositories {
 }
 
 dependencies {
+    // Server
     implementation("io.ktor:ktor-server-core:1.6.3")
     implementation("io.ktor:ktor-server-netty:1.6.3")
     implementation("ch.qos.logback:logback-classic:1.2.5")
 
+    // Discord API
     implementation("com.discord4j:discord4j-core:3.1.7")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.5.1")
 
+    // Database
+    implementation("org.jetbrains.exposed:exposed-core:0.31.1")
+    implementation("org.jetbrains.exposed:exposed-java-time:0.31.1")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.31.1")
+    implementation("org.postgresql:postgresql:42.2.23")
+    implementation("com.zaxxer:HikariCP:5.0.0")
+
+    // Image processing
     implementation("org.imgscalr:imgscalr-lib:4.2")
 }
 
--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,3 +1,6 @@
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import data.Visits
 import discord4j.common.util.Snowflake
 import discord4j.core.DiscordClient
 import discord4j.core.GatewayDiscordClient
@@ -6,6 +9,7 @@ import discord4j.gateway.intent.Intent
 import discord4j.gateway.intent.IntentSet
 import discord4j.rest.util.Image
 import io.ktor.application.*
+import io.ktor.features.*
 import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
@@ -13,17 +17,26 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.reactor.awaitSingle
 import kotlinx.coroutines.reactor.awaitSingleOrNull
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
+import org.jetbrains.exposed.sql.transactions.transaction
 import utils.getOrNull
 import java.io.ByteArrayOutputStream
+import java.time.Instant
 import javax.imageio.ImageIO
+import kotlin.concurrent.thread
+
 
 val guildId: Snowflake = Snowflake.of(System.getenv("GUILD_ID"))
 val token = System.getenv("TOKEN") ?: error("TOKEN environment variable is not set.")
 val port = System.getenv("PORT")?.toIntOrNull() ?: 8080
 
-fun createBot() : GatewayDiscordClient {
+private fun createBot() : GatewayDiscordClient {
     return DiscordClient.create(token)
         .gateway()
         .setEnabledIntents(IntentSet.of(Intent.GUILD_PRESENCES))
@@ -32,16 +45,51 @@ fun createBot() : GatewayDiscordClient {
         .block() ?: error("GatewayDiscordClient.login().block() returned null.")
 }
 
+private fun createDatabaseConnection() : Database {
+    val databaseUrl = System.getenv("JDBC_DATABASE_URL") ?: error("DATABASE_URL environment variable is not set.")
+    val config = HikariConfig().apply {
+        jdbcUrl = databaseUrl
+    }
+    val ds = HikariDataSource(config)
+    val db =  Database.connect(ds)
+
+    Runtime.getRuntime().addShutdownHook(thread(start = false) {
+        ds.close()
+    })
+
+    transaction {
+        SchemaUtils.create(Visits)
+    }
+
+    return db
+}
+
+private suspend fun logVisit(call: ApplicationCall, requestedUserId: Long) {
+    newSuspendedTransaction(Dispatchers.IO) {
+        Visits.insert {
+            it[ip] = call.request.origin.remoteHost
+            it[userId] = requestedUserId
+            it[timestamp] = Instant.now()
+        }
+    }
+}
+
 fun main() {
 
     val bot = createBot()
+    createDatabaseConnection()
 
     embeddedServer(Netty, port = port) {
+        install(XForwardedHeaderSupport)
+
         routing {
             get("/{id}.png") {
                 val id = call.parameters["id"]?.toLongOrNull() ?: return@get call.respond(HttpStatusCode.BadRequest)
                 val guild = bot.getGuildById(guildId).awaitSingleOrNull() ?: return@get call.respond(HttpStatusCode.NotFound)
                 val member = guild.getMemberById(Snowflake.of(id)).awaitSingleOrNull() ?: return@get call.respond(HttpStatusCode.NotFound)
+
+                // Log this request
+                launch { logVisit(call, id) }
 
                 val presence = member.presence.awaitSingle()
 
